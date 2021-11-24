b7c06adb1a02c2c8212aad388d61f469dc7be63d
Joel Santiago <jsant288@fiu.edu>
2021-11-24 18:08:07 +0000

    added spaces between array values

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
 
 let pronoun = ["the", "our"];
@@ -10,7 +9,7 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       document.querySelector("#content").innerHTML +=
-        "<li>" + pronoun[i] + adj[j] + noun[k] + "</li>";
+        "<li>" + pronoun[i] + " " + adj[j] + " " + noun[k] + "</li>";
     }
   }
 }
